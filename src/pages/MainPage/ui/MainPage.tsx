227dbc18f3ffc3c4bb045ede58ecaa3ae24334b1
roman-styazhkin <rmn.styazhkin@gmail.com>
2025-06-18 02:35:33 +0300

    added-not-found-page

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,13 +1,13 @@
-import {useTranslation} from "react-i18next";
+import { useTranslation } from 'react-i18next';
 
 const MainPage = () => {
-    const { t } = useTranslation('main');
+  const { t } = useTranslation('main');
 
-    return (
-        <div className="app__page">
-            <h1>{t('Главная страница')}</h1>
-        </div>
-    );
+  return (
+    <div className="app__page">
+      <h1>{t('Главная страница')}</h1>
+    </div>
+  );
 };
 
 export default MainPage;
--- src/shared/config/routerConfig/routerConfig.tsx
+++ src/shared/config/routerConfig/routerConfig.tsx
@@ -1,25 +1,33 @@
-import {RouteProps} from "react-router-dom";
-import {MainPage} from "pages/MainPage";
-import {AboutPage} from "pages/AboutPage";
+import { RouteProps } from 'react-router-dom';
+import { MainPage } from 'pages/MainPage';
+import { AboutPage } from 'pages/AboutPage';
+import { NotFoundPage } from 'pages/NotFoundPage';
 
 export enum AppRoutes {
     MAIN = 'main',
     ABOUT = 'about',
+    NOT_FOUND = 'not_found'
 }
 
 export const RoutePaths: Record<AppRoutes, string> = {
-    [AppRoutes.MAIN]: '/',
-    [AppRoutes.ABOUT]: '/about'
-}
+  [AppRoutes.MAIN]: '/',
+  [AppRoutes.ABOUT]: '/about',
+  [AppRoutes.NOT_FOUND]: '*',
+};
 
 export const routeConfig: Record<AppRoutes, RouteProps> = {
-    [AppRoutes.MAIN]: {
-        path: RoutePaths.main,
-        element: <MainPage />
-    },
+  [AppRoutes.MAIN]: {
+    path: RoutePaths.main,
+    element: <MainPage />,
+  },
 
-    [AppRoutes.ABOUT]: {
-        path: RoutePaths.about,
-        element: <AboutPage />
-    }
-}
+  [AppRoutes.ABOUT]: {
+    path: RoutePaths.about,
+    element: <AboutPage />,
+  },
+
+  [AppRoutes.NOT_FOUND]: {
+    path: RoutePaths.not_found,
+    element: <NotFoundPage />,
+  },
+};
