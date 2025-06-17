fbc4ee32b98e3dcce79beefcd7f81e8b87ab2198
roman-styazhkin <rmn.styazhkin@gmail.com>
2025-06-18 00:07:08 +0300

    added-stylelint

--- src/shared/ui/AppLink/ui/AppLink.tsx
+++ src/shared/ui/AppLink/ui/AppLink.tsx
@@ -1,6 +1,6 @@
-import {FC} from 'react';
-import {classNames} from 'shared/lib/classNames';
-import {Link, LinkProps} from "react-router-dom";
+import { FC } from 'react';
+import { classNames } from 'shared/lib/classNames';
+import { Link, LinkProps } from 'react-router-dom';
 import cls from './AppLink.module.scss';
 
 export enum AppLinkTheme {
@@ -14,21 +14,21 @@ interface AppLinkProps extends LinkProps {
 }
 
 export const AppLink: FC<AppLinkProps> = (props) => {
-    const {
-        className,
-        children,
-        to,
-        theme = AppLinkTheme.PRIMARY,
-        ...otherProps
-    } = props;
+  const {
+    className,
+    children,
+    to,
+    theme = AppLinkTheme.PRIMARY,
+    ...otherProps
+  } = props;
 
-    return (
-        <Link
-            to={to}
-            className={classNames(cls.AppLink, {}, [className, cls[theme]])}
-            {...otherProps}
-        >
-            {children}
-        </Link>
-    );
+  return (
+    <Link
+      to={to}
+      className={classNames('', {}, [className, cls[theme]])}
+      {...otherProps}
+    >
+      {children}
+    </Link>
+  );
 };
