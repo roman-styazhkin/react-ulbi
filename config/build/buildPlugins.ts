68ee257349a034300b8d6f6484a19bfb88288b0b
roman-styazhkin <rmn.styazhkin@gmail.com>
2025-06-24 04:28:19 +0300

    added-configuration-for-main.yml

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -18,13 +18,13 @@ export function buildPlugins({ paths, isDev } : BuildOptions): webpack.WebpackPl
     new webpack.DefinePlugin({
       __IS_DEV__: JSON.stringify(isDev),
     }),
-    new BundleAnalyzerPlugin({
-      openAnalyzer: false,
-    }),
   ];
 
   if (isDev) {
     // plugins.push(new ReactRefreshWebpackPlugin());
+    plugins.push(new BundleAnalyzerPlugin({
+      openAnalyzer: false,
+    }));
     plugins.push(new webpack.HotModuleReplacementPlugin());
   }
 
