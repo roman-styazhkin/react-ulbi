23ecd5706ea33b25e083a1e94691ee2e6a6f4b06
roman-styazhkin <rmn.styazhkin@gmail.com>
2025-06-30 18:42:29 +0300

    componentRender.tsx

--- src/widgets/Sidebar/ui/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar.test.tsx
@@ -1,15 +1,15 @@
 import { fireEvent, screen } from '@testing-library/react';
-import { renderWithTranslation } from 'shared/lib/tests/renderWithTranslation/renderWithTranslation';
+import { componentRender } from 'shared/lib/tests/componentRender/componentRender';
 import { Sidebar } from './Sidebar';
 
 describe('Sidebar', () => {
   test('to be in the document', () => {
-    renderWithTranslation(<Sidebar />);
+    componentRender(<Sidebar />);
     expect(screen.getByTestId('sidebar-element')).toBeInTheDocument();
   });
 
   test('toggle collapse', () => {
-    renderWithTranslation(<Sidebar />);
+    componentRender(<Sidebar />);
     expect(screen.getByTestId('sidebar-element')).toBeInTheDocument();
     const sidebarToggler = screen.getByTestId('sidebar-toggler');
     fireEvent.click(sidebarToggler);
